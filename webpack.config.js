215798a1e52b32e5b9783878f2f1474e1182ca3e
Josh Pablico <jbpablico27@gmail.com>
2024-10-03 14:33:34 -0500

    Setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,17 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	mode : 'development',
-	entry: './src/index.js',
+	entry: './src/Script/index.js',
 	output: {
 		filename: "main.js",
 		path: path.resolve(__dirname, 'dist'),
 		clean: true
-	}
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "./src/template.html"
+		}),
+	],
 }
