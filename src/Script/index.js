e094d48c3cf10347941c0cf32e44afbee8a13c7d
Josh Pablico <jbpablico27@gmail.com>
2024-10-03 15:49:37 -0500

    JS

--- src/Script/index.js
+++ src/Script/index.js
@@ -1,4 +1,6 @@
 import '../Style/styles.scss';
+import { Header } from './Components/header.js';
 import { greeting } from './greeting.js';
 
+document.body.appendChild(Header());
 console.log(greeting);
---
+++ webpack.config.cjs
@@ -0,0 +1,47 @@
+import path from 'path';
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+export default {
+  mode: "development",
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/template.html"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
